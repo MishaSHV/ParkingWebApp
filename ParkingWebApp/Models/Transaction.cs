db2dde7f2a7d1e557cb7b253d55650d5308fe4d6
Shvidkoy Mychailo <origami690@gmail.com>
2018-05-14 18:32:43 +0300

    Add all model classes to project

---
+++ ParkingWebApp/Models/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkingWebApp.Models
+{
+    public class Transaction
+    {
+        public DateTime DTtransaction { get; set; }
+        public int CarId { get; set; }
+        public decimal AmountMoney { get; set; }
+    }
+}
