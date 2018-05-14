5753c91bbf8514c571e3d6189871942cc3a1b70a
Shvidkoy Mychailo <origami690@gmail.com>
2018-05-14 19:02:40 +0300

    Add templates to Parking and Transactions controllers

--- ParkingWebApp/Controllers/ParkingController.cs
+++ ParkingWebApp/Controllers/ParkingController.cs
@@ -4,35 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using ParkingWebApp.Models;
 
 namespace ParkingWebApp.Controllers
 {
     [Produces("application/json")]
     [Route("api/[controller]")]
-    public class CarsController : ControllerBase
+    public class ParkingController : Controller
     {
-        // GET: api/Cars
+        // GET: api/Parking
         [HttpGet]
-        public IEnumerable<Car> Get()
+        public IEnumerable<string> Get()
         {
-            return new Car[] { new Car(){ Id= 1, Balance=23.0M, Ctype=CarType.Bus}, new Car(){ Id = 1, Balance = 340, Ctype = CarType.Motocycle } };
+            return new string[] { "value1", "value2" };
         }
 
-        // GET: api/Cars/5
+        // GET: api/Parking/5
         [HttpGet("{id}", Name = "Get")]
         public string Get(int id)
         {
             return "value";
         }
         
-        // POST: api/Cars
+        // POST: api/Parking
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
         
-        // PUT: api/Cars/5
+        // PUT: api/Parking/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
--- ParkingWebApp/Controllers/TransactionsController.cs
+++ ParkingWebApp/Controllers/TransactionsController.cs
@@ -4,35 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using ParkingWebApp.Models;
 
 namespace ParkingWebApp.Controllers
 {
     [Produces("application/json")]
     [Route("api/[controller]")]
-    public class CarsController : ControllerBase
+    public class TransactionsController : Controller
     {
-        // GET: api/Cars
+        // GET: api/Transactions
         [HttpGet]
-        public IEnumerable<Car> Get()
+        public IEnumerable<string> Get()
         {
-            return new Car[] { new Car(){ Id= 1, Balance=23.0M, Ctype=CarType.Bus}, new Car(){ Id = 1, Balance = 340, Ctype = CarType.Motocycle } };
+            return new string[] { "value1", "value2" };
         }
 
-        // GET: api/Cars/5
+        // GET: api/Transactions/5
         [HttpGet("{id}", Name = "Get")]
         public string Get(int id)
         {
             return "value";
         }
         
-        // POST: api/Cars
+        // POST: api/Transactions
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
         
-        // PUT: api/Cars/5
+        // PUT: api/Transactions/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
