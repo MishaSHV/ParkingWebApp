193bf4de3102da75b81a3857be0bce6645aed291
Shvidkoy Mychailo <origami690@gmail.com>
2018-05-14 18:07:55 +0300

    Add Models folder and car model

---
+++ ParkingWebApp/Models/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkingWebApp.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public decimal Balance { get; set; }
+        public CarType Ctype { get; set; }
+    }
+
+    public enum CarType
+    {
+        Passenger, Truck, Bus, Motocycle
+    }
+}
